c057978f1a482fba537f8856f7a1062c08d0199a
Patrick Paysant <patrick.paysant@linagora.com>
2015-09-07 16:42:08 +0200

    Fix notice about function declaration.

--- command/populate.php
+++ command/populate.php
@@ -11,6 +11,7 @@
 namespace OCA\Dashboard\Command;
 
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputOption;
